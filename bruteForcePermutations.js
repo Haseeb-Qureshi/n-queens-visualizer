771f5344e024c18db8668a37696cb063813eb016
Haseeb Qureshi <haseeb@haseebq.com>
2016-01-03 17:36:23 -0600

    Start brute force permutations

---
+++ bruteForcePermutations.js
@@ -0,0 +1,54 @@
+/*jslint node: true */
+"use strict";
+function bruteForcePermutations(size) {
+  var board = [];
+  for (var i = 0; i < size; i++) board.push(i);
+  for (var j = 0; j < factorial(size); j++) {
+    board = nextPerm(board);
+    if (noConflictsOn(board)) return board;
+  }
+  return false;
+}
+
+function factorial(size) {
+  var prod = 1;
+  for (var i = 1; i <= size; i++) prod *= i;
+  return prod;
+}
+
+function nextPerm(perm) {
+  var swap1 = swapPoint(perm);
+  if (swap1 === -1) return perm;
+  var swap2 = smallestGreaterOnRight(perm, swap1);
+  swap(perm, swap1, swap2);
+  return perm.slice(0, swap1 + 1).concat(sortSlice(perm, swap1 + 1));
+}
+
+function swapPoint(arr) {
+  for (var i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] < arr[i + 1]) break;
+  }
+  return i;
+}
+
+function smallestGreaterOnRight(arr, point) {
+  var minIdx = point + 1;
+  for (var i = point + 1; i < arr.length; i++) {
+    if ( arr[i] > arr[point] && arr[i] < arr[minIdx]) {
+      minIdx = i;
+    }
+  }
+  return minIdx;
+}
+
+function swap(arr, x, y) {
+  var temp = arr[x];
+  arr[x] = arr[y];
+  arr[y] = temp;
+}
+
+function sortSlice(arr, idx) {
+  return arr.slice(idx || 0, arr.length).sort(function (el1, el2) {
+    return el1 > el2 ? 1 : -1;
+  });
+}
--- queens.js
+++ queens.js
@@ -13,7 +13,6 @@ Board.prototype.bruteForce = function () {
 
       if (this.grid[i][j] === undefined) {
         this.placeQueen(i, j);
-        this.bruteForce();
       }
     }
   }
@@ -38,43 +37,6 @@ Board.prototype.removeLastQueen = function () {
   this.eachAttackableAt(x, y, this.eraseAttacks.bind(this));
   this.grid[x][y] = undefined;
   this.placedQueens--;
-};
-
-Board.prototype.markAttack = function (x, y) {
-  this.grid[x][y] = this.grid[x][y] || this.placedQueens;
-};
-
-Board.prototype.eraseAttacks = function (x, y) {
-  if (this.grid[x][y] === this.placedQueens) this.grid[x][y] = undefined;
-};
-
-Board.prototype.eachAttackableAt = function (x, y, callback) {
-  // row
-  for (var i = 0; i < this.size; i++) {
-    if (i === y) continue;
-    callback(x, i);
-  }
-  // column
-  for (i = 0; i < this.size; i++) {
-    if (i === x ) continue;
-    callback(i, y);
-  }
-  // diags left-to-right
-  var x1 = x > y ? x - y : 0;
-  var y1 = x > y ? 0 : y - x;
-  while (x1 < this.size && y1 < this.size) {
-    if (x1 !== x) callback(x1, y1);
-    x1 += 1;
-    y1 += 1;
-  }
-  // diags right-to-left
-  var x2 = x + y <= this.size ? x + y : this.size;
-  var y2 = x + y <= this.size ? 0 : x + y - this.size;
-  while (x2 >= 0 && x2 < this.size && y2 < this.size) {
-    if (x2 !== x) callback(x2, y2);
-    x2 -= 1;
-    y2 += 1;
-  }
 };
 
 Board.prototype.render = function () {
