29206739ca05cea72cbfb0ef15dffbe64c7646f3
Haseeb Qureshi <haseeb@haseebq.com>
2016-01-30 18:10:15 -0800

    Create board store

--- src/stores/board-store.js
+++ src/stores/board-store.js
@@ -2,3 +2,41 @@ var AppDispatcher = require('../dispatcher/app-dispatcher');
 
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+
+var n = 8;
+
+var board = new Array(n);
+
+var BoardStore = assign({}, EventEmitter.prototype, {
+  emitChange: function () {
+    this.emit('change');
+  },
+
+  addChangeListener: function (callback) {
+    this.on('change', callback);
+  },
+
+  removeChangeListener: function (callback) {
+    this.removeListener('change', callback);
+  },
+
+  _moveQueen: function (queenId, toRow) {
+    board[queenId] = toRow;
+  },
+
+  getBoard: function () {
+    return board;
+  },
+});
+
+AppDispatcher.register(function (action) {
+  switch (action.actionType) {
+    case "MOVE_QUEEN":
+      BoardStore._moveQueen(action.data[0], action.data[1]);
+      BoardStore.emitChange();
+      break;
+    default:
+  }
+});
+
+module.exports = BoardStore;
