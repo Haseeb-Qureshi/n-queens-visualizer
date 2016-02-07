64a3b22543ec9176e7ea405356c623a41a17e360
Haseeb Qureshi <haseeb@haseebq.com>
2016-02-07 14:52:43 -0800

    Add attribution and Github links

--- src/components/container.js
+++ src/components/container.js
@@ -17,6 +17,8 @@ var Container = React.createClass({
   render: function () {
     return (
       <div className="container">
+        <h1 className="title"> N-Queens Visualizer </h1>
+        <h2 className="attribution">by <a href="https://github.com/Haseeb-Qureshi">Haseeb Qureshi</a></h2>
         <Board />
         <AlgoPanel algos={algos} />
         <div className="row"/>
